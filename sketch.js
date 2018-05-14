3008510ec5c951054bebcebe78b3a8c61117a496
DiMoSe <36212269+DiMoSe@users.noreply.github.com>
2018-05-14 02:08:52 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ LIFE CYCLE METHODS
 */
 
 function setup() {
-  createCanvas(1920, 800);
+  createCanvas(1920, 825);
   var button = select("#movie");
   var button2 = select("#actor");
   button.mousePressed(movieInfo);
